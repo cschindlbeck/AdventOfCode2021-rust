3a634889ec58468149d702f739d70f0e013a40ea
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2021-12-21 12:46:47 +0100

    minimal working example file reader

--- aoc-2021-1/src/main.rs
+++ aoc-2021-1/src/main.rs
@@ -1,23 +1,16 @@
-//mod read_input;
-use std::fs;
+use std::{fs, str::SplitWhitespace};
 
 fn main() {
-    println!("Hello, world!");
-    //read_input::read_input();
-    //read_input::read_a_file();
 
     let filename: &str = "src/input.txt";
-
-    // --snip--
-    println!("In file {}", filename);
-
     let contents: String =
-        fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let bla = contents.split_whitespace();
-    let mut vec = Vec::<i64>::new();
+        fs::read_to_string(filename)
+            .expect("Something went wrong reading the file");
+    let splitwhitespace: SplitWhitespace = contents.split_whitespace();
     
-    for b in bla {
-        vec.push(b.parse::<i64>().unwrap());
+    let mut vec: Vec<i64> = Vec::<i64>::new();
+    for v in splitwhitespace {
+        vec.push(v.parse::<i64>().unwrap());
     }
 
     println!("With text:\n{}", vec[0]);
